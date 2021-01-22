1d7a2b6927b9f143cd3c7e1f1f79a0bb997f4329
lijiang1993 <1933098912@qq.com>
2021-01-22 15:25:22 +0800

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * 头像显示
+     * @param string $size
+     * @return string
+     */
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }

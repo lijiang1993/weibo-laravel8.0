b2616cc812d6ba2ea046d36496ef65cc704333f0
lijiang1993 <1933098912@qq.com>
2021-01-22 21:40:41 +0800

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,12 +12,23 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store', 'index']
         ]);
 
-        $this->middleware('guest',[
+        $this->middleware('guest', [
             'only' => ['create'],
         ]);
+    }
+
+
+    /**
+     * 用户列表中心
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function index()
+    {
+        $users = User::paginate(6);
+        return view('users.index',compact('users'));
     }
 
     public function create()
@@ -62,7 +73,7 @@ class UsersController extends Controller
      */
     public function edit(User $user)
     {
-        $this->authorize('update',$user);
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
@@ -75,7 +86,7 @@ class UsersController extends Controller
      */
     public function update(User $user, Request $request)
     {
-        $this->authorize('update',$user);
+        $this->authorize('update', $user);
         $this->validate($request, [
             'name' => 'required|unique:users|max:50',
             'password' => 'nullable|confirmed|min:6',

d317c1aca1cb64d3add3ab692c2ebafda4d7f273
lijiang1993 <1933098912@qq.com>
2021-01-22 17:56:31 +0800

    完成用户注册

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,11 +20,22 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
+        //验证用户提交信息
         $this->validate($request, [
             'name' => 'required|unique:users|max:50',
             'email' => 'required|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
+
+        //储存用户信息
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success','欢迎，您将在这里开启一段新的旅程');
+        return redirect()->route('users.show',[$user]);
     }
 
 }

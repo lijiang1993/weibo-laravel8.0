a78bcfe2198c1091de89340f2db9a9102bc1586b
lijiang1993 <1933098912@qq.com>
2021-01-23 20:22:12 +0800

    发布微博

--- routes/web.php
+++ routes/web.php
@@ -33,13 +33,16 @@ Route::delete('logout','SessionsController@destroy')->name('logout');
 Route::get('signup/confirm/{token}','UsersController@confirmEmail')->name('confirm_email');
 
 
-
 //忘记密码
 Route::get('password/reset',  'PasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email',  'PasswordController@sendResetLinkEmail')->name('password.email');
 
 Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset',  'PasswordController@reset')->name('password.update');
+
+
+//微博相关操作路由
+Route::resource('statuses','StatusesController',['only'=>['store','destroy']]);
 
 
 

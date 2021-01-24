8f315932aff2c27dba8f95c152d5037dc25b461b
lijiang1993 <1933098912@qq.com>
2021-01-24 19:38:35 +0800

    关注用户动态流

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -10,13 +10,18 @@ class UserPolicy
     use HandlesAuthorization;
 
 
-    public function update(User $currentUser,User $user)
+    public function update(User $currentUser, User $user)
     {
         return $currentUser->id === $user->id;
     }
 
-    public function destroy(User $currentUser,User $user)
+    public function destroy(User $currentUser, User $user)
     {
         return $currentUser->id && $currentUser->id !== $user->id;
+    }
+
+    public function follow(User $currentUser, User $user)
+    {
+        return $currentUser->id !== $user->id;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,11 @@ Route::get('/signup','UsersController@create')->name('signup');
 Route::resource('users','UsersController');
 Route::get('/users/{user}/edit','UsersController@edit')->name('users.edit');
 
-Route::get('users/{user}/followers','UsersController@followers')->name('users.followers');
-Route::get('users/{user}/followings','UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers','UsersController@followers')->name('users.followers');
+Route::get('/users/{user}/followings','UsersController@followings')->name('users.followings');
+
+Route::post('/users/followers/{user}','FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}','FollowersController@destroy')->name('followers.destroy');
 
 
 Route::get('login','SessionsController@create')->name('login');
